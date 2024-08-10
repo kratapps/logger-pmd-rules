b6302f9628167dfe87e718430a88b53282d10b35
kratoon <ondrej@kratochvil.dev>
2024-08-10 09:52:58 +0200

    Improve tests.

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,32 +2,33 @@
 
 result="$(cat out/test_results.json | jq .result)"
 
-success=$(echo "${result}" | jq 'map(select(.fileName | endswith("classes/AuraEnabledMissingFinally.cls")) | select(.violations | length == 1 and .[0].ruleName == "AuraEnabledMethodShouldPublishLogsInFinallyBlock")) | length == 1')
-if ! ${success} ; then
-  echo "Error: There must be exactly one violation with ruleName 'AuraEnabledMethodShouldPublishLogsInFinallyBlock' for classes/AuraEnabledMissingFinally.cls." >&2
-  exit 1
-fi
+exactly_one_violation() {
+  local class_name="$1"
+  local rule_name="$2"
+  success=$(echo "${result}" | jq --arg class_name "$class_name" --arg rule_name "$rule_name" \
+    'map(select(.fileName | endswith("classes/" + $class_name + ".cls")) 
+    | select(.violations | length == 1 and .[0].ruleName == $rule_name)) | length == 1')
+  if ! ${success} ; then
+    echo "Error: There must be exactly one violation with ruleName '$rule_name' for classes/$class_name.cls." >&2
+    exit 1
+  fi
+}
 
-success=$(echo "${result}" | jq 'map(select(.fileName | endswith("classes/AuraEnabledMissingPublishInFinally.cls")) | select(.violations | length == 1 and .[0].ruleName == "AuraEnabledMethodShouldPublishLogsInFinallyBlock")) | length == 1')
-if ! ${success} ; then
-  echo "Error: There must be exactly one violation with ruleName 'AuraEnabledMethodShouldPublishLogsInFinallyBlock' for classes/AuraEnabledMissingPublishInFinally.cls." >&2
-  exit 1
-fi
+no_violation() {
+  local class_name="$1"
+  success=$(echo "${result}" | jq --arg class_name "$class_name" \
+    'map(select(.fileName | endswith("classes/" + $class_name + ".cls")) | select(.violations)) | length == 0')
+  if ! ${success} ; then
+    echo "Error: There should be no violation for classes/$class_name.cls." >&2
+    violations=$(echo "${result}" | jq --arg class_name "$class_name" \
+        'map(select(.fileName | endswith("classes/" + $class_name + ".cls")) | .violations)' )
+    echo "Violations: ${violations}" >&2
+    exit 1
+  fi
+}
 
-success=$(echo "${result}" | jq 'map(select(.fileName | endswith("classes/AuraEnabledMissingTryCatch.cls")) | select(.violations | length == 1 and .[0].ruleName == "AuraEnabledMethodShouldPublishLogsInFinallyBlock")) | length == 1')
-if ! ${success} ; then
-  echo "Error: There must be exactly one violation with ruleName 'AuraEnabledMethodShouldPublishLogsInFinallyBlock' for classes/AuraEnabledMissingTryCatch.cls." >&2
-  exit 1
-fi
-
-success=$(echo "${result}" | jq 'map(select(.fileName | endswith("classes/LoggerInitClassDoesntMatchClassName.cls")) | select(.violations | length == 1 and .[0].ruleName == "LoggerInitClassMatchesClassName")) | length == 1')
-if ! ${success} ; then
-  echo "Error: There must be exactly one violation with ruleName 'LoggerInitClassMatchesClassName' for classes/LoggerInitClassDoesntMatchClassName.cls." >&2
-  exit 1
-fi
-
-success=$(echo "${result}" | jq 'map(select(.fileName | endswith("classes/ClassNoViolations.cls"))) | length == 0')
-if ! ${success} ; then
-  echo "Error: There should be no violation for classes/ClassNoViolations.cls." >&2
-  exit 1
-fi
+no_violation "ClassNoViolations"
+exactly_one_violation "AuraEnabledMissingFinally" "AuraEnabledMethodShouldPublishLogsInFinallyBlock"
+exactly_one_violation "AuraEnabledMissingPublishInFinally" "AuraEnabledMethodShouldPublishLogsInFinallyBlock"
+exactly_one_violation "AuraEnabledMissingTryCatch" "AuraEnabledMethodShouldPublishLogsInFinallyBlock"
+exactly_one_violation "LoggerInitClassDoesntMatchClassName" "LoggerInitClassMatchesClassName"
